b4e2962c3708caef0d2b3f736eae9911493ad249
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-06-17 14:32:53 +0200

    add menu.html menu.css menu.js and modify links in index.html

---
+++ menu.js
@@ -0,0 +1,10 @@
+// Drop-down (show/hide) menu
+function youSeeMe(menu) {
+    let menuVisible = document.querySelector(`#bottom${menu}`)
+  
+      if (menuVisible.style.display === "none") {
+        menuVisible.style.display = "block";
+      } else {
+        menuVisible.style.display = "none";
+      }
+    }
