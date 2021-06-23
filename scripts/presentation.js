c0caa0991fba84e53bb1a9f45dcf16343388368b
Guillaume Lequin <guillaume.lequin713@gmail.com>
2021-06-23 14:38:28 +0200

    minor changes

--- scripts/presentation.js
+++ scripts/presentation.js
@@ -1,10 +1,16 @@
+/* animation title */
+
+
 const animMainTitle = document.querySelector("h1");
 
 let typeHOne = new Typewriter(animMainTitle, {
-    delay: 200,
+    delay: 100,
     cursor: ""
   })
   
   typeHOne
-  .typeString("Qui sommes nous")
+  .typeString("L'Odyssey, Qui sommes nous")
   .start()
+
+
+
